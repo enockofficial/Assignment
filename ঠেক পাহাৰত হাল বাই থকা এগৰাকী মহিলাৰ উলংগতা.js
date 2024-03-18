004e5020254d1f357100c51c200872a74b95970c
SMG CEO <163293857+enockofficial@users.noreply.github.com>
2024-03-18 22:29:55 +0300

    Update ঠেক পাহাৰত হাল বাই থকা এগৰাকী মহিলাৰ উলংগতা.js

--- ঠেক পাহাৰত হাল বাই থকা এগৰাকী মহিলাৰ উলংগতা.js
+++ ঠেক পাহাৰত হাল বাই থকা এগৰাকী মহিলাৰ উলংগতা.js
@@ -1,3 +1,4 @@
+
         $(document).ready(() => {
             $('#convertButton').on('click', () => {
                 const element = document.getElementById('reviewAssignment');
@@ -96,6 +97,12 @@
             $('#reviewAssignment').show();
         }
    
-    if (document.referrer !== "https://enockofficial.github.io/home/index.html" && document.referrer !== "") {
+  
+
+        window.onload = function() {
+    // Angalia ikiwa ukurasa umeingiwa moja kwa moja kutoka "home.page"
+    if(document.referrer !== "https://enockofficial.github.io/home/index.html") {
+        // Ikiwa sio hivyo, rudisha mtumiaji kwenye "home.page"
         window.location.href = "https://enockofficial.github.io/SMG-OFFICE-2024/login.php.html";
-        }
+    }
+}
