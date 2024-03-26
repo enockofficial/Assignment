a1425d1d7823e17ce24aeb623e716186bde68ad2
SMG CEO <163293857+enockofficial@users.noreply.github.com>
2024-03-26 16:13:37 +0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+
+          window.onload = function() {
+    // Angalia ikiwa ukurasa umeingiwa moja kwa moja kutoka "home.page"
+    if(document.referrer !== "https://enockofficial.github.io/home/index.html") {
+        // Ikiwa sio hivyo, rudisha mtumiaji kwenye "home.page"
+        window.location.href = "https://enockofficial.github.io/SMG-OFFICE-2024/login.php.html";
+    }
+
