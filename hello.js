8b543be18ddd687929329f5e5b6ff8ad39bceeb3
SMG CEO <163293857+enockofficial@users.noreply.github.com>
2024-04-07 18:45:45 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
 // Check if the page is opened from the correct link
 if (document.referrer !== "https://enockofficial.github.io/SMG-OFFICE-2024/home_page.html") {
-    // Redirect to another page if opened from incorrect link
+    // Redirect to the correct page
     window.location.href = "https://enockofficial.github.io/T.I.A/php.html";
 }
+
+//official ceo .smg
