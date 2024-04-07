83e9a7bc45ef9739e6b5044c4854b26b83d69559
SMG CEO <163293857+enockofficial@users.noreply.github.com>
2024-04-07 18:43:51 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,5 @@
-
-          window.onload = function() {
-    // Angalia ikiwa ukurasa umeingiwa moja kwa moja kutoka "home.page"
-    if(document.referrer !== "https://enockofficial.github.io/SMG-OFFICE-2024/home_page.html") {
-        // Ikiwa sio hivyo, rudisha mtumiaji kwenye "home.page"
-        window.location.href = "https://enockofficial.github.io/T.I.A/php.html";
-    }
-
+// Check if the page is opened from the correct link
+if (document.referrer !== "https://enockofficial.github.io/SMG-OFFICE-2024/home_page.html") {
+    // Redirect to another page if opened from incorrect link
+    window.location.href = "https://enockofficial.github.io/T.I.A/php.html";
+}
